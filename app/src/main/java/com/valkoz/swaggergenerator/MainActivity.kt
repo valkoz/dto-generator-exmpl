accf8afe261369c1bb20f7c94a8405b0b619bf70
valkoz <kozlov.valent@gmail.com>
2020-11-24 17:12:42 +0300

    add generated code to sourceSets

--- app/src/main/java/com/valkoz/swaggergenerator/MainActivity.kt
+++ app/src/main/java/com/valkoz/swaggergenerator/MainActivity.kt
@@ -2,11 +2,14 @@ package com.valkoz.swaggergenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.uber.feature.dto.ExpandedPet
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ExpandedPet(name = "name", id = 0L)
     }
 }
---
+++ payments/src/main/java/com/valkoz/payments/ExampleClass.kt
@@ -0,0 +1,10 @@
+package com.valkoz.payments
+
+import com.uber.payments.dto.PriceEstimate
+
+class ExampleClass {
+
+    fun doSmth() {
+        PriceEstimate()
+    }
+}
